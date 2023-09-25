d36c01a7fa4cf4aa00a997c3c1ab6ae37368df6d
narsis123 <145971535+narsis123@users.noreply.github.com>
2023-09-25 12:07:19 +0330

    Add files via upload

---
+++ TemplateAddDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer.DTO
+{
+	public class TemplateAddDto  
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public IEnumerable<TemplateItemAddDto> Items { get; set; }
+	}
+}
